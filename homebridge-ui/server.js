58a6d2a2abaf37a5fc89c0aabbafc9ff6946f74b
Ung Sing Wong <96530237+uswong@users.noreply.github.com>
2024-02-08 15:33:22 +0800

    Added more CF setup options

--- homebridge-ui/server.js
+++ homebridge-ui/server.js
@@ -37,20 +37,20 @@ class UiServer extends HomebridgePluginUiServer
       if (payload.ip !== "") 
       {
          console.log(`Processing BondBridge device: ${payload.ip}, token: ${payload.token}, debug: ${payload.debug}`);
-         console.log(`Setup instruction: ${payload.setupOption}`);
-         console.log(`Timer setup instruction: ${payload.timerSetup}`);
+         console.log(`Setup instruction: ${payload.CFsetupOption}`);
+         console.log(`Timer setup instruction: ${payload.CFtimerSetup}`);
       }
       if (payload.ip2 !== "") 
       {
          console.log(`Processing BondBridge device: ${payload.ip2}, token: ${payload.token2}, debug: ${payload.debug2}`);
-         console.log(`Setup instruction: ${payload.setupOption2}`);
-         console.log(`Timer setup instruction: ${payload.timerSetup2}`);
+         console.log(`Setup instruction: ${payload.CFsetupOption2}`);
+         console.log(`Timer setup instruction: ${payload.CFtimerSetup2}`);
       }
       if (payload.ip3 !== "") 
       {
          console.log(`Processing BondBridge device: ${payload.ip3}, token: ${payload.token3}, debug: ${payload.debug3}`);
-         console.log(`Setup instruction: ${payload.setupOption3}`);
-         console.log('Timer setup instruction:', payload.timerSetup3);
+         console.log(`Setup instruction: ${payload.CFsetupOption3}`);
+         console.log('Timer setup instruction:', payload.CFtimerSetup3);
       }
 
       try {
@@ -59,7 +59,7 @@ class UiServer extends HomebridgePluginUiServer
 
          //This spawns a child process which runs a bash script
          const spawnSync = require('child_process').spawnSync;
-         let FeedBack = spawnSync(ConfigCreator_shPath, [payload.ip,payload.token,payload.setupOption,payload.timerSetup,payload.debug,payload.ip2,payload.token2,payload.setupOption2,payload.timerSetup2,payload.debug2,payload.ip3,payload.token3,payload.setupOption3,payload.timerSetup3,payload.debug3,BondBridge_shPath], {encoding: 'utf8'});
+         let FeedBack = spawnSync(ConfigCreator_shPath, [payload.ip,payload.token,payload.CFsetupOption,payload.CFtimerSetup,payload.debug,payload.ip2,payload.token2,payload.CFsetupOption2,payload.CFtimerSetup2,payload.debug2,payload.ip3,payload.token3,payload.CFsetupOption3,payload.CFtimerSetup3,payload.debug3,BondBridge_shPath], {encoding: 'utf8'});
          let feedback = `${ FeedBack.stdout.replace(/\n*$/, "")}`
 
          // return data to the ui
