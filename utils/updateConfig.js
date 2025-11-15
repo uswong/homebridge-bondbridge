09dcdf01102be4190f0065ade47084d62e8c1f17
Ung Sing Wong <96530237+uswong@users.noreply.github.com>
2025-11-15 17:33:29 +0800

    Added ability to cache the updated config

--- utils/updateConfig.js
+++ utils/updateConfig.js
@@ -1,19 +1,49 @@
 // Update myplaceConfig
 const { createBondbridgeConfig } = require("./createBondbridgeConfig");
+const { getBondHost } = require("./getBondHost");
+const { readConfig } = require("./readConfig");
+
 const chalk = require("chalk");
 
-async function updateConfig(config, log, pluginPath) {
+async function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-  // Run ConfigCreator to update the BondBridge config
+async function updateConfig(config, log, storagePath, pluginPath) {
   log.info(chalk.yellow('Running createBondbridgeConfig...'));
+
+  // resolve ID for IP or use IP directly if specified
+  let IPs = [];
+
   try {
-    const bbConfig = await createBondbridgeConfig(config, pluginPath);
-    log.info("✅ DONE! createBondbridgeConfig completed successfully!");
+    IPs = await getBondHost(config, log);
+  } catch (err) {
+    log.error(`❌ ${err.message}`);
+    const existingConfig = readConfig( storagePath, log );
+    if (existingConfig) {
+      log.warn('⚠️ Proceed with existing config from cache — all existing accessories will be restored.');
+      return existingConfig;
+    }
+    log.warn('⚠️ Proceed with original config — no accessories will be created and cached accessories will be removed.');
+    return config;
+  }
+  let noOfDevices = IPs.length;
+  let noOfDevicesProcessed = IPs.filter(ip => ip !== "undefined").length;;
+
+  try {
+    const bbConfig = await createBondbridgeConfig(config, IPs, log, pluginPath);
+    log.info(`✅ DONE! createBondbridgeConfig completed successfully for ${noOfDevicesProcessed}/${noOfDevices} device(s)!`);
     log.debug('Updated Bondbridge config:\n' + JSON.stringify(bbConfig));
     return bbConfig;
   } catch (err) {
     log.error(`❌ ${err.message}`);
-    log.warn('⚠️  Proceed with original config — no accessories will be created and cached accessories will be removed.');
+    const existingConfig = readConfig( storagePath, log );
+
+    if (existingConfig && Array.isArray(existingConfig.accessories)) {
+      log.warn('⚠️ Proceed with existing config from cache — all existing accessories will be restored.');
+      return existingConfig;
+    }
+    log.warn('⚠️ Proceed with original config — no accessories will be created and any cached accessories will be removed.');
     return config;
   }
 }
